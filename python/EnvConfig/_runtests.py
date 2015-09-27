07a18710960c27fa9a2ba029b91274ff743a5186
Benedikt Hegner <hegner@cern.ch>
2015-09-27 13:00:12 +0200

    remove gaudi-specific parts

--- python/EnvConfig/_runtests.py
+++
@@ -1,7 +0,0 @@
-"""
-Wrapper to run the tests.
-"""
-import os
-from nose import main
-
-main(defaultTest=os.path.normpath(__file__+"/../.."))
--- python/env.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python2.6
-import EnvConfig
-EnvConfig.Script().main()
